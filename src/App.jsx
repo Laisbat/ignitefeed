6228d3de9fcf02c0c1d8c9ca371ced0542cc0b6f
Lais <laisbatistapereira@gmail.com>
2023-07-09 00:16:30 -0300

    :sparkles: Create Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { Header } from "./components/Header";
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
     <>
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+      </div>
     </>
   );
 }
